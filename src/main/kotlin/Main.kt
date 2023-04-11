1e731750a976159f7d2e92b63b8f007234f458d3
Faith-Omenta <faithbomenta@gmail.com>
2023-04-11 15:36:53 +0300

    principle

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,65 @@
+fun main() {
+    println("hello,world")
+    greetings()
+}
+fun greetings(){
+    println("good morning")
+
+    var name=("Faith")
+    println(name)
+    var age=(45)
+    println(age)
+    var phoneNumber=("0746807381")
+    println(phoneNumber)
+    var weight=(88.3)
+    println(weight)
+    var isKenyan=true
+    var citizen=("Kenyan")
+    println(citizen)
+
+    person("Glenah")
+
+    println(modulus(73,4))
+
+    println(addition(43,54,65,76))
+
+    println(multiply(23,26))
+
+    println(subract(786,543))
+
+    fact("I love my family")
+}
+//VARIABLES AND DATATYPE
+//You are creating an app to capture student records. Some of the data you will
+//capture includes full name, age, phone number, weight in kg and citizen. For the
+//citizen field you will track whether a student is a Kenyan or not.
+
+//Create and invoke a function that takes in a name and prints out “Hello ${name}” e.g
+//given “Ada” it will print out “Hello Ada” (2 points)
+fun person(name:String){
+    println("Hello " +name)
+}
+//Create and invoke a function that given 2 numbers returns the remainder of their
+//division (3 points)
+fun modulus(num:Int,num1:Int):Int{
+    var remainder=num%num1
+    return remainder
+}
+//Create and invoke function that returns the sum of any given 4 numbers (3 points)
+fun addition(num2:Int,num3:Int,num4:Int,num5:Int):Int{
+    var sum=num2+num3+num4+num5
+    return sum
+}
+fun multiply(num6:Int,num7:Int):Int{
+    var product=num6*num7
+    return product
+
+}
+fun subract(num8:Int,num9:Int):Int{
+    var sub=num8-num9
+    return sub
+}
+//Create and invoke a function that prints out an interesting fact about yourself
+fun fact(sentence:String){
+    println(sentence)
+}
